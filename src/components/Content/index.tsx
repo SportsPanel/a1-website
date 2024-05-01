7213b024c3dc81760cae5dbcccdcab24ec44911b
DevanshBatra20 <devanshbatra15@gmail.com>
2024-05-01 09:27:01 +0530

    Created about us and content components

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const Content: React.FC<any> = () => {
+  return (
+    <section id="content">
+      <div className="flex flex-col bg-white justify-center items-center p-20 bg-[url(./assets/images/content-bg.png)]">
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-row">
+            <p className="text-black font-light text-5xl mr-3">Why</p>
+            <p className="text-black font-semibold text-5xl">A1 Sports</p>
+          </div>
+          <p className="w-2/3 text-black font-light text-base text-center mt-5">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also 
+          </p>
+        </div>
+        <div className="flex flex-row mt-10">
+          <div className="flex flex-col items-center justify-evenly">
+            <p className="text-primary font-semibold text-7xl">Investment</p>
+            <p className="text-black font-light text-base text-center">
+              We are offering users to buy and sell shares of listed athletes on
+              our platform's. Growth of the shares depends on 3Ps -
+              Performance,Partnership and Presence.
+            </p>
+          </div>
+          <div className="h-auto border-[1px] border-gray mx-5"></div>
+          <div className="flex flex-col">
+            <div className="flex flex-col mb-5">
+              <p className="text-primary font-semibold text-4xl mb-2">IPO</p>
+              <p className="text-black font-light text-base text-left">
+                Athlete’s shares will be launched as Initial Public Offering on
+                specific dates which will be announced prior to the dates.
+              </p>
+            </div>
+            <div className="border-gray border-[1px] mb-5"></div>
+            <div className="flex flex-col">
+              <p className="text-primary font-semibold text-4xl mb-2">
+                Experts Talk
+              </p>
+              <p className="text-black font-light text-base text-left">
+                Athlete’s shares will be launched as Initial Public Offering on
+                specific dates which will be announced prior to the dates.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Content;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,37 +1,89 @@
-
-import React from "react"
+import React from "react";
 
 const Header: React.FC<any> = () => {
-
-   const scrollToSection = (id: string) => {
-    const element = document.getElementById(id)
+  const scrollToSection = (id: string) => {
+    const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({behavior: "smooth"})
+      element.scrollIntoView({ behavior: "smooth" });
     }
-   }
-   
-    return(
+  };
+
+  return (
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-black px-10 py-4">
         <div className="flex flex-wrap flex-row items-center justify-between mx-auto max-w-screen-xl">
-        <h1 className="text-primary font-bold text-3xl">A1 Sports</h1>
-              <ul className="flex flex-row font-normal">
-                <li>
-                    <a 
-                    href="#about"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      scrollToSection("about")
-                    }}
-                    className="block py-2 pr-4 pl-3 duration-200 text-white hover:text-gray">
-                         About Us                      
-                    </a>
-                </li>
-              </ul>
-           </div>
+          <h1 className="text-primary font-bold text-3xl">A1 Sports</h1>
+          <ul className="flex flex-row font-normal">
+            <li>
+              <a
+                href="#about"
+                onClick={(e) => {
+                  e.preventDefault();
+                  scrollToSection("about");
+                }}
+                className="block py-2 pr-4 pl-3 duration-200 text-white hover:text-gray"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#content"
+                onClick={(e) => {
+                  e.preventDefault();
+                  scrollToSection("content");
+                }}
+                className="block py-2 pr-4 pl-3 duration-200 text-white hover:text-gray"
+              >
+                Content
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                onClick={(e) => {
+                  e.preventDefault();
+                  scrollToSection("about");
+                }}
+                className="block py-2 pr-4 pl-3 duration-200 text-white hover:text-gray"
+              >
+                Reports
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                onClick={(e) => {
+                  e.preventDefault();
+                  scrollToSection("about");
+                }}
+                className="block py-2 pr-4 pl-3 duration-200 text-white hover:text-gray"
+              >
+                Blu Mart
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                onClick={(e) => {
+                  e.preventDefault();
+                  scrollToSection("about");
+                }}
+                className="block py-2 pr-4 pl-3 duration-200 text-white hover:text-gray"
+              >
+                FAQ
+              </a>
+            </li>
+          </ul>
+          <div className="flex w-36 h-12 border-[1px] rounded-lg border-white justify-center items-center mr-5 hover:border-primary">
+            <p className="text-white font-semibold text-lg hover:text-primary">
+              About Us
+            </p>
+          </div>
+        </div>
       </nav>
     </header>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
